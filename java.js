0bf7691e75bed18dec9fccb7d447d65524dd8063
Oier35-prog <oier.aguadoopendeusto.es>
2026-03-22 11:29:36 +0100

    dos funcines de java y algunos cambios en HTML y en CSS

--- java.js
+++ java.js
@@ -9,3 +9,37 @@ function contacto() {
         alert("Completa los campos monimos de :CLIENTE , EMAIL EMPRESA y ¿DE QUE TRATA EL PROYECTO?. Para poder recivir la informacion minima de cara el presupuesto.")
     }
 }
+let usuariosRegistrados = [];
+let contrasenaRegistrados = [];
+
+function crearUsuarios() {
+    let nuevoUsuario = document.getElementsByName("nuevo_usuario")[0].value;
+    let nuevoEmail = document.getElementsByName("email_nuevo")[0].value;
+    let nuevaContrasena = document.getElementsByName("nueva_contraseña")[0].value;
+
+    if (nuevaContrasena != "" && nuevoEmail != "" && nuevoUsuario != "") {
+        usuariosRegistrados.push(nuevoUsuario);
+        contrasenaRegistrados.push(nuevaContrasena);
+        alert("Nuevo usuario creado: " + nuevoUsuario);
+    } else {
+        alert("ERROR, completa todos los requisitos");
+    }
+}
+
+function iniciarSesion() {
+    let Usuario = document.getElementsByName("usuario")[0].value;
+    let Contrasena = document.getElementsByName("contraseña")[0].value;
+    let OK = false;
+
+    for (let i = 0; i < usuariosRegistrados.length; i++) {
+        if (usuariosRegistrados[i] == Usuario && contrasenaRegistrados[i] == Contrasena) {
+            OK = true;
+        }
+    }
+
+    if (OK == true) {
+        alert("Inicio de sesion completado");
+    } else {
+        alert("ERROR; usuario o contraseña incorrectos");
+    }
+}
